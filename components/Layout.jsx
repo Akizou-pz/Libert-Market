2c0d447c5cc637d3de987e735ffaeca3e26732c3
AKIZOU <pakizourichard@gmail.com>
2023-05-29 08:54:03 +0000

    ajout de l'index html

---
+++ components/Layout.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+
+const Layout = () => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title> Liberte Market </title>
+      </Head>
+
+      <header>
+        <Navbar />
+      </header>
+
+      <main className='main-container'>
+        EMPTY
+      </main>
+
+      <Footer>
+        <Footer />
+      </Footer>
+
+    </div>
+  )
+}
+
+export default Layout
